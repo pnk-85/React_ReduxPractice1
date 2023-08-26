1b8a057a9a1fa09f4066a4564bed647b2486f075
pnk-85 <pnkghogare27@gmail.com>
2023-08-26 18:08:22 +0530

    making network call and store data to firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { interfaceActions } from './Store/interfaceSlice';
+import Notification from './components/UI/Notification';
 
 
+let isInitial = true;
 
 function App() {
 
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async() => {
+      dispatch(interfaceActions.showNotification({
+        status: 'pending',
+        title: 'sending....',
+        message: 'sending cart data!'
+      }))
+      const response = await fetch('https://react-http-project-dcdba-default-rtdb.firebaseio.com/cart.json', 
+    {
+      method : 'PUT',
+      body : JSON.stringify(cart)
+    });
+
+    if(!response.ok){
+      throw new Error('cart data failed');
+    }
+
+    dispatch(interfaceActions.showNotification({
+      status : 'success',
+      title: 'success',
+      message: 'sent data successfully'
+    }))
+    };
+
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(interfaceActions.showNotification({
+        status : 'error',
+        title: 'error',
+        message: 'sending cart data failed'
+      }))
+    })
+    
+  },[cart, dispatch]);
 
   return (
-    <Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
+    
   );
 }
 
--- src/Store/interfaceSlice.js
+++ src/Store/interfaceSlice.js
@@ -2,10 +2,13 @@ import { CreateSlice, createSlice } from "@reduxjs/toolkit";
 
 const interfaceSlice = createSlice({
     name : 'interface',
-    initialState : { cartIsVisible : false},
+    initialState : { cartIsVisible : false, notification : null},
     reducers : {
         toggle (state) {
             state.cartIsVisible = !state.cartIsVisible
+        },
+        showNotification(state, action) {
+            state.notification = {status : action.payload.status, title: action.payload.title,message: action.payload.message }
         }
     }
 });
